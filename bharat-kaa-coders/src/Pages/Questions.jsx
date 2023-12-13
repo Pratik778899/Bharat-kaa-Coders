138ff9053aa94820aad90fafd29dc5196b938ac4
Saadansari234 <saadansari3212@gmail.com>
2023-12-13 19:55:19 +0530

    added hover effect in questions

--- bharat-kaa-coders/src/Pages/Questions.jsx
+++ bharat-kaa-coders/src/Pages/Questions.jsx
@@ -9,9 +9,9 @@ const Questions = () => {
         <div className="text-3xl md:text-6xl font-semibold green-grade whitespace-nowrap">
           Questions
         </div>
-        <div className="gap-4 item-center flex justify-center flex-col items-center relative">
+        <div className=" gap-4 item-center flex justify-center flex-col items-center relative">
           <div
-            className="text-white text-sm md:text-lg border-solid
+            className="text-white text-sm md:text-lg border-solid hover:shadow-sm hover:shadow-white
                              border-gray-500 border-2 rounded-md p-2 md:p-5 w-11/12 md:w-5/5 cursor-pointer "
           >
             <p>
@@ -20,7 +20,7 @@ const Questions = () => {
             </p>
           </div>
           <div
-            className="text-white text-sm md:text-lg border-solid
+            className="text-white text-sm md:text-lg border-solid hover:shadow-sm hover:shadow-white
                              border-gray-500 border-2 rounded-md p-2 md:p-5 w-11/12 md:w-5/5  cursor-pointer "
           >
             <p>
@@ -29,7 +29,7 @@ const Questions = () => {
             </p>
           </div>
           <div
-            className="text-white text-sm md:text-lg border-solid
+            className="text-white text-sm md:text-lg border-solid hover:shadow-sm hover:shadow-white
                              border-gray-500 border-2 rounded-md p-2 md:p-5 w-11/12 md:w-5/5  cursor-pointer "
           >
             <p>
@@ -38,7 +38,7 @@ const Questions = () => {
             </p>
           </div>
           <div
-            className="text-white text-sm md:text-lg border-solid
+            className="text-white text-sm md:text-lg border-solid hover:shadow-sm hover:shadow-white
                              border-gray-500 border-2 rounded-md p-2 md:p-5 w-11/12 md:w-5/5  cursor-pointer "
           >
             <p>
@@ -47,7 +47,7 @@ const Questions = () => {
             </p>
           </div>
           <div
-            className="text-white text-sm md:text-lg border-solid
+            className="text-white text-sm md:text-lg border-solid hover:shadow-sm hover:shadow-white
                              border-gray-500 border-2 rounded-md p-2 md:p-5 w-11/12 md:w-5/5  cursor-pointer "
           >
             <p>
